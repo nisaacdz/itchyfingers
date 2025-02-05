fdb165be5c2e8e321da3857ce408f818b9402e80
Isaac Dzikum <nisaacdz@gmail.com>
2025-02-05 00:27:55 +0000

    initial setup

---
+++ app/config/index.ts
@@ -0,0 +1,9 @@
+const config = {
+    apps: {
+      core: process.env.API_BASE_URL || "http://localhost:4000",
+    },
+    client: "itchy-fingers-scratcher",
+  };
+  
+  export default config;
+  
