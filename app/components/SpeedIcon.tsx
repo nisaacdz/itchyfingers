08b9c93c320c15a24cd8e4d210e31d07436f0192
Isaac Newton Dzikum <nisaacdz@gmail.com>
2025-02-07 08:16:18 +0000

    Add speed icons

--- app/components/SpeedIcon.tsx
+++ app/components/SpeedIcon.tsx
@@ -8,8 +8,9 @@ type SpeedIconProps = {
 
 const SpeedIcon = ({ speed, styles, ref }: SpeedIconProps) => {
   // draw different icons based on speed
+  const img_src = speed < 30 ? "/tortoise.svg" : speed < 50 ? "/rabbit.svg" : speed < 80 ? "/dog.svg" : "/rocket.svg";
   return (
-    <img src="/tortoise.svg" className="size-10" style={styles} ref={ref} />
+    <img src={img_src} className="size-10" style={styles} ref={ref} />
   );
 };
 
