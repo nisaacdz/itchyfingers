ac6b2c61367e10dbc7a4923997b67f9846a98827
Isaac <nisaacdz@gmail.com>
2025-04-02 10:09:24 +0000

    Update dkdls

--- components/custom/AuthLoader.tsx
+++ components/custom/AuthLoader.tsx
@@ -1,7 +1,7 @@
 export const AuthLoader = () => {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center bg-muted">
-      <span className="dots-loader"></span>;
+      <span className="dots-loader"></span>
     </div>
   );
 };
