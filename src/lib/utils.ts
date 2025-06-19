896996d02d564201222b045f436fcb1a72fc4891
Isaac <nisaacdz@gmail.com>
2025-06-19 02:14:00 +0000

    Add util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
+import { TournamentData, TournamentStatus } from "@/types/api";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getStatus(data: TournamentData): TournamentStatus {
+  if (data.endedAt) return "ended";
+  if (data.startedAt) return "started";
+  return "upcoming";
 }
